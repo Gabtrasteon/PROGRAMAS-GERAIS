351790b0339fee2277d65184b7fa4840137f3ec6
Gabtraste <gabtraste007@gmail.com>
2025-07-15 14:34:51 -0300

    pe

--- PROGRAMAS_HTML/JS/EX1/script.js
+++ PROGRAMAS_HTML/JS/EX1/script.js
@@ -1,19 +1,8 @@
-var n1 = Number(prompt(`Digite um valor`))
-var n2 = Number(prompt(`Digite outro valor`))
-var soma = n1+n2
-var menos = n1-n2
-var mult = n1*n2
-var div = n1/n2
-var expo = n1**n2
-var rest = n1%n2
-alert(`${n1} + ${n2} = ${soma}`)
-
-alert(`${n1} - ${n2} = ${menos}`)
-
-alert(`${n1} x ${n2} = ${mult}`)
-
-alert(`${n1} / ${n2} = ${div}`)
-
-alert(`${n1} ^ ${n2} = ${expo}`)
-
-alert(`${n1} % ${n2} = ${rest}`)
+var idade = Number(prompt(`Digite um valor`))
+var teste = idade>=18 ?'VOCÊ É DE MAIOR':'VOCÊ É DE MENOR'
+document.getElementById('box').innerText = teste
+if(teste=='VOCÊ É DE MAIOR'){
+    document.getElementById('box1').innerText = 'voce pode ver coisas cabulosas hehehe'
+}else{
+    document.getElementById('box1').innerText = 'vai assistir pepa pig'
+}
