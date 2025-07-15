15737562e146a41d42c051ba2887cb1b9cbd496c
Gabtraste <gabtraste007@gmail.com>
2025-07-15 14:41:23 -0300

    tste

--- PROGRAMAS_HTML/JS/EX1/script.js
+++ PROGRAMAS_HTML/JS/EX1/script.js
@@ -2,7 +2,7 @@ var idade = Number(prompt(`Digite um valor`))
 var teste = idade>=18 ?'VOCÊ É DE MAIOR':'VOCÊ É DE MENOR'
 document.getElementById('box').innerText = teste
 if(teste=='VOCÊ É DE MAIOR'){
-    document.getElementById('box1').innerText = 'voce pode ver coisas cabulosas hehehe'
+    document.getElementById('box1').innerHTML = '<a href="https://youtube.com" target="_blank">clique aqui</a>'
 }else{
-    document.getElementById('box1').innerText = 'vai assistir pepa pig'
+    document.getElementById('box1').innerHTML = '<a href="https://disney.com" target="_blank">clique aqui</a>'
 }
