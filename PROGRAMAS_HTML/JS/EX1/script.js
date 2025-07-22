6a8802ffb0418440cbf486e07b75b38a66ad355a
Gabtraste <gabtraste007@gmail.com>
2025-07-22 14:48:25 -0300

    update

--- PROGRAMAS_HTML/JS/EX1/script.js
+++
@@ -1,8 +0,0 @@
-var idade = Number(prompt(`Digite um valor`))
-var teste = idade>=18 ?'VOCÊ É DE MAIOR':'VOCÊ É DE MENOR'
-document.getElementById('box').innerText = teste
-if(teste=='VOCÊ É DE MAIOR'){
-    document.getElementById('box1').innerHTML = '<a href="https://youtube.com" target="_blank">clique aqui</a>'
-}else{
-    document.getElementById('box1').innerHTML = '<a href="https://disney.com" target="_blank">clique aqui</a>'
-}
